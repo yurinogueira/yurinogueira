2ff953ecb9b1dee7c7f870e45332e7dd1119151c
yurinogueira <yurinogueira@id.uff.br>
2021-05-14 15:14:56 -0300

    Teste Brabo

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -2,17 +2,20 @@
   <div class="about">
     <Navbar></Navbar>
     <h1>This is an about page</h1>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'About',
 
   components: {
-    Navbar
+    Navbar,
+    Footer
   },
 }
 </script>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <Navbar></Navbar>
     <img alt="Vue logo" src="../assets/logo.png">
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'Home',
 
   components: {
-    Navbar
+    Navbar,
+    Footer
   },
 }
 </script>
