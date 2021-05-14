f505030eb11b482c27d10710461b3f0920c2c3e0
yurinogueira <yurinogueira@id.uff.br>
2021-05-13 22:22:42 -0300

    teste

--- app/scripts/gh-pages-deploy.js
+++ app/scripts/gh-pages-deploy.js
@@ -6,8 +6,7 @@ const fs = require("fs");
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         // eslint-disable-next-line no-console
         console.log("Building started...");
-        await execa("yarn", ["serve", "build"]);
-        // await execa("yarn", ["build"]);
+        await execa("yarn", ["build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
