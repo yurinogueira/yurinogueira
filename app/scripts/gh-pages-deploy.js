6be1a8ef993a5d2f694ce88b202ddfc261d79385
yurinogueira <yurinogueira@id.uff.br>
2021-05-14 16:31:22 -0300

    Try AGAIN

--- app/scripts/gh-pages-deploy.js
+++ app/scripts/gh-pages-deploy.js
@@ -9,7 +9,7 @@ const fs = require("fs");
         await execa("yarn", ["build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
-        await execa('cp', ['./CNAME ./dist/CNAME']);
+        await execa('cp', ['./CNAME', './dist/CNAME']);
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
