1ae2dde6b7bd4ac3b6ff479b7a57fb112ba25dac
yurinogueira <yurinogueira@id.uff.br>
2021-05-14 21:15:00 -0300

    Aumentando a home

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
     <Navbar></Navbar>
-    <article>
-      <section class="card m-3">
+    <article class="container">
+      <section class="mt-3">
         <div class="row">
           <div class="col-sm">
             <img alt="Vue logo" height="200" width="200" src="../assets/logo.png">
           </div>
+
           <div class="col-sm mt-auto mb-auto p-4">
             Este <strong class="text-success">site</strong> e todos outros projetos de minha autoria
             são de código aberto e podem ser acessados pelo <strong>GitHub</strong>,
@@ -15,10 +16,39 @@
           </div>
         </div>
       </section>
-      <section class="m-3">
-        <p>
 
-        </p>
+      <section class="mt-3 mb-3">
+        <div class="container">
+          <div class="row">
+            <div class="card col-sm d-block">
+              <img alt="IEEE 754" width="200" height="200" src="../assets/ieee754.png">
+              <h5 class="card-title">Ponto Flutuante</h5>
+              <p class="card-text">
+                Possui dúvidas em Ponto Flutuante? Não se desespere
+                isso é normal, clique no botão abaixo, talvez isso
+                possa ti ajudar. <span class="secondary">:)</span>
+              </p>
+              <router-link to="/" tag="button" type="button" class="btn btn-outline-success w-100 mb-2">
+                Acessar
+              </router-link>
+            </div>
+
+            <div class="col-sm-1"></div>
+
+            <div class="card col-sm d-block">
+              <img alt="Conversões de Bases" width="200" height="200" src="../assets/base-convertion.png">
+              <h5 class="card-title">Conversões de Bases</h5>
+              <p class="card-text">
+                Acha que errou alguma coisa na hora de alterar a
+                base de um número? Acho que vale apena você dá
+                uma verificada, não custa nada.
+              </p>
+              <router-link to="/" tag="button" type="button" class="btn btn-outline-success w-100 mb-2">
+                Acessar
+              </router-link>
+            </div>
+          </div>
+        </div>
       </section>
     </article>
     <Footer></Footer>
