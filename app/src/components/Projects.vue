faeec23048820d1bd07261ea26053270432db23c
Yuri Nogueira <yurinogueira@id.uff.br>
2023-02-20 15:07:00 -0300

    chore: adjust first index.

--- app/src/components/Projects.vue
+++ app/src/components/Projects.vue
@@ -28,7 +28,7 @@ export default defineComponent({
   name: "Projects",
   data() {
     return {
-      activeName: ref("Expeer"),
+      activeName: ref("Siteware"),
       projectsData: projectsJson
     }
   }
